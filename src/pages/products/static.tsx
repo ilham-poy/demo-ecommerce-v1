3351570a6a85849060145b9983096223fd3437c3
Rafli Ilham Aditia <ilhamrafli732@gmail.com>
2025-09-05 00:50:56 +0700

    fixing ssr and ssg

--- src/pages/products/static.tsx
+++ src/pages/products/static.tsx
@@ -1,34 +1,34 @@
-import Card from "@/views/Products/Card/card";
-type productType = {
-    id: number,
-    name: string,
-    price: number,
-    image: string,
-    category: string
+// import Card from "@/views/Products/Card/card";
+// type productType = {
+//     id: number,
+//     name: string,
+//     price: number,
+//     image: string,
+//     category: string
 
-}
-// Ini Server Side rendering
-const StaticSideGeneration = (props: { products: productType[] }) => {
-    const { products } = props;
-    return (
-        <div>
-            <h1> Halamaan Static Side Generation</h1>
-            <p>Klo mau liat perubahan harus build ulang</p>
-            <Card products={products}></Card>
-        </div>
-    )
-}
-export default StaticSideGeneration;
+// }
+// // Ini Server Side rendering
+// const StaticSideGeneration = (props: { products: productType[] }) => {
+//     const { products } = props;
+//     return (
+//         <div>
+//             <h1> Halamaan Static Side Generation</h1>
+//             <p>Klo mau liat perubahan harus build ulang</p>
+//             <Card products={products}></Card>
+//         </div>
+//     )
+// }
+// export default StaticSideGeneration;
 
-export async function getStaticProps() {
-    //fetch api
-    const res = await fetch('http://localhost:3000/api/product')
-    const response = await res.json();
+// export async function getStaticProps() {
+//     //fetch api
+//     const res = await fetch('http://localhost:3000/api/product')
+//     const response = await res.json();
 
-    return {
-        props: {
-            products: response.data
-        },
-        revalidate: 10
-    }
-}
+//     return {
+//         props: {
+//             products: response.data
+//         },
+//         revalidate: 10
+//     }
+// }
