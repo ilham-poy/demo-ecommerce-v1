ac57908f6ad5c81b58756af06debaf6b68e2f2b7
Rafli Ilham Aditia <ilhamrafli732@gmail.com>
2025-10-18 11:01:15 +0700

    add seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,16 @@ export default function App({ Component, pageProps: { session, pageProps } }: Ap
   return (
     <SessionProvider session={session}>
       <Head>
+        <title>Nasywa Art Space | Rajutan & Seni Crochet</title>
+        <meta name="description" content="Toko online rajutan dan crochet handmade. Temukan karya seni benang yang unik dan berkualitas." />
+        <meta name="keywords" content="rajutan, crochet, handmade, seni benang, nasywa art, toko rajutan" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Nasywa Art Space | Rajutan & Seni Crochet" />
+        <meta property="og:description" content="Toko online rajutan dan crochet handmade. Temukan karya seni benang yang unik dan berkualitas." />
+        <meta property="og:image" content="/default-og.jpg" />
+        <meta property="og:type" content="website" />
+        <link rel="canonical" href="https://nasywaartspace.vercel.app/" />
         <link rel="icon" href="/logo-nasywa.png" />
-        <title>Nasywa Art Space</title>
       </Head>
       <AppShell>
         <Component {...pageProps} />
